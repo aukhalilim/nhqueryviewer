d8b157edf87780929549d52966b434f5281e3b2c
bs.stack <bs.stack@gmail.com>
2010-03-18 23:03:24 +0000

    Added support - now logs session factory statistics

---
+++ NHibernateProfiler.Common/Entity/PreparedStatementParameter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace NHibernateProfiler.Common.Entity
+{
+    /// <summary>
+    /// bstack @ 19/12/2009
+    /// Prepared statement parameter
+    /// </summary>
+    public class PreparedStatementParameter
+    {
+		/// <summary>
+		/// Id
+		/// </summary>
+		public Guid Id { get; set; }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Type
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Value
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
